967da2cf49e34d94141aa07d26ce6f19c9d21b0d
Sanjana2000 <sanjana100@gmail.com>
2018-04-26 00:28:49 -0700

    checkpoint16 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
-{
-  $("#album-title").text(album.title);
-  $("img#album-cover-art").attr("src", album.albumArtUrl);
-}
+  {
+    $('#album-title').text(album.title);
+    $('img#album-cover-art').attr('src', 'assets/images/album_covers/01.png');
+    $("#release-info").text(album.releaseInfo);
+    $(".artist").text(album.artist);
+  }
