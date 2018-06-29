c105c64aea4a769289b79976f2a14890e59dbdab
Sanjana2000 <sanjana100@gmail.com>
2018-06-28 19:07:29 -0700

    add images

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -1,13 +1,13 @@
 const album = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  releaseInfo: '1909 Spanish Records',
+  title: 'NoshMusic',
+  artist: 'Sonically season your dish !',
+  releaseInfo: 'Various Artists',
   albumArtUrl: 'assets/images/album_covers/01.png',
   songs: [
-    { title: 'Blue', duration: '161.71', soundFileUrl: 'assets/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', soundFileUrl: 'assets/music/green.mp3' },
-    { title: 'Red', duration: '268.45', soundFileUrl: 'assets/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', soundFileUrl: 'assets/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', soundFileUrl: 'assets/music/magenta.mp3' }
+    { title: 'Mindfulness', duration: '161.71', soundFileUrl: 'assets/music/blue.mp3' },
+    { title: 'Nourishment', duration: '103.96', soundFileUrl: 'assets/music/green.mp3' },
+    { title: 'Awareness', duration: '268.45', soundFileUrl: 'assets/music/red.mp3' },
+    { title: 'Preparing meal', duration: '153.14', soundFileUrl: 'assets/music/pink.mp3' },
+    { title: 'Indulgence', duration: '374.22', soundFileUrl: 'assets/music/magenta.mp3' }
   ]
 };
--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,6 +1,6 @@
 {
   $('#album-title').text(album.title);
-  $('img#album-cover-art').attr('src', 'assets/images/album_covers/01.png');
+  $('img#album-cover-art').attr('src', 'assets/images/album_covers/NoshPics/Nosh1.jpeg');
 $("#release-info").text(album.releaseInfo);
 $(".artist").text(album.artist);
 }
